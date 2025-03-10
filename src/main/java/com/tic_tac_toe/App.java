f6ce4e627d891b333f1756eb780894427e2a75fc
edmimi <mirisaor02@gmail.com>
2025-03-10 09:16:20 +0100

    basic information

--- src/main/java/com/tic_tac_toe/App.java
+++ src/main/java/com/tic_tac_toe/App.java
@@ -1,13 +1,13 @@
 package com.tic_tac_toe;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to Tic Tac Toe !! " );
+
+        GameBoard board = new GameBoard();
+        board.showBoard();
+
     }
 }
