42a119a1bd66c78878627bb400f75a8007f7f5be
Israel Espin <israel.espin92@gmail.com>
2025-03-13 09:33:28 +0100

    boardgame bad

--- src/main/java/com/tic_tac_toe/GameBoard.java
+++ src/main/java/com/tic_tac_toe/GameBoard.java
@@ -12,16 +12,16 @@ public class GameBoard {
     private void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+                board[i][j] = '_';
             }
         }
     }   
 
     public void printBoard() {
-        System.out.println("-------------");
+        
 
         for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
+
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " | ");
             }
