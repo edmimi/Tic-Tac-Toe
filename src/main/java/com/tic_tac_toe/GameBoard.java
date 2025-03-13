fe3686308e5cd5263555e4edc2370a47028438af
edmimi <mirisaor02@gmail.com>
2025-03-13 09:20:46 +0100

    First Page

--- src/main/java/com/tic_tac_toe/GameBoard.java
+++ src/main/java/com/tic_tac_toe/GameBoard.java
@@ -11,11 +11,20 @@ public class GameBoard {
     }
     
     private void initializeBoard(){
-         for (int i=0;i<3;i++){
-            for (int j=0;j<3;j++){
+         for (int i=0;i<3; i++){
+            for (int j=0; j<3; j++){
                 board[i][j] = '-';
             }
          }
+    }
+
+    public void showBoard() { 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
