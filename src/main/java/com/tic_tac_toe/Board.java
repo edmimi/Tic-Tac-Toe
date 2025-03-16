08e29b297129468fb77428be2e7ead59fa9b3af5
Israel Espin <israel.espin92@gmail.com>
2025-03-16 20:27:03 +0100

    Comments deleted and player O name changed

--- src/main/java/com/tic_tac_toe/Board.java
+++ src/main/java/com/tic_tac_toe/Board.java
@@ -52,14 +52,14 @@ public class Board {
     }
 
     public boolean checkWinner(char player) {
-        // Check rows and columns
+    
         for (int i = 0; i < 3; i++) {
             if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
                 (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
                 return true;
             }
         }
-        // Check diagonals
+        
         if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
             (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
             return true;
--- src/main/java/com/tic_tac_toe/Main.java
+++ src/main/java/com/tic_tac_toe/Main.java
@@ -3,7 +3,7 @@ package com.tic_tac_toe;
 public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Game turn :", 'X');
-        Player player2 = new Player("Shift O :", 'O');
+        Player player2 = new Player("Game turn :", 'O');
         Game game = new Game(player1, player2);
         game.play();
     }
